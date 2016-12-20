8ef61cee015ed7bff713ff854fd77833fb8b3fa2
minato <bledikv@gmail.com>
2016-12-20 19:17:08 +0200

    Add description for 'Sum all numbers in a range' challenge.

--- intermediate-algorithm-scripting/rangesum.js
+++ intermediate-algorithm-scripting/rangesum.js
@@ -1,3 +1,10 @@
+/*
+ Description
+ ***********
+ Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
+ In other words, return the symmetric difference of the two arrays.
+ 
+ */
 function sumAll(arr) {
 	var res = 0,
 		maxValue = arr.reduce(function(a, b) {
