6039754cf02709658816b16e225684ec2f399a7f
minato <bledikv@gmail.com>
2016-12-20 19:47:01 +0200

    Add solution for 'Missing letter' challenge.

---
+++ intermediate-algorithm-scripting/missingletter.js
@@ -0,0 +1,36 @@
+/*
+ Description
+ ***********
+ Find the missing letter in the passed letter range and return it.
+
+ If all letters are present in the range, return undefined.
+
+ Example
+ -------
+ fearNotLetter("abce") should return "d"
+ fearNotLetter("abcdefghjklmno") should return "i"
+ fearNotLetter("bcd") should return undefined.
+
+
+ Helpful String methods
+ ----------------------
+ String.prototype.charCodeAt() -> The static String.fromCharCode() method returns a string created by using the 
+ specified sequence of Unicode values.
+
+ String.fromCharCode() -> The charCodeAt() method returns an integer between 0 and 65535 representing the UTF-16 
+ code unit at the given index.
+
+ */
+function fearNotLetter(str) {
+	var prevCharCode = str[0].charCodeAt();
+
+	for (var i = 1; i < str.length; i++) {
+		var curCharCode = str[i].charCodeAt();
+		if (curCharCode - prevCharCode > 1) {
+			return String.fromCharCode(prevCharCode + 1);
+		}
+		prevCharCode = curCharCode;
+	}
+
+	return undefined;
+}
